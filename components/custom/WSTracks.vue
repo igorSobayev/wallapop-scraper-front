0eb00778d408eefe920c8dfa3ec2173f10e4bd6e
Igor <isobayev@gmail.com>
2024-01-20 11:55:18 +0100

    Tracks table component

---
+++ components/custom/WSTracks.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+const route = useRoute()
+
+const people = [
+    {
+        id: 1,
+        name: 'Lindsay Walton',
+        title: 'Front-end Developer',
+        email: 'lindsay.walton@example.com',
+        role: 'Member'
+    }
+]
+
+onNuxtReady(async () => {
+
+})
+</script>
+
+<template>
+    <div class="border">
+        <UTable :rows="people" />
+    </div>
+</template>
