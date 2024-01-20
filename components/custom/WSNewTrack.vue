3c04e226a0f04ba73d7e104b0273e58cb4096b23
Igor <isobayev@gmail.com>
2024-01-20 11:55:12 +0100

    New track component

---
+++ components/custom/WSNewTrack.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+const route = useRoute()
+
+onNuxtReady(async () => {
+
+})
+</script>
+
+<template>
+    <div class="">
+        <div class="w-full text-3xl mb-2">
+            New Track
+        </div>
+        <div class="w-full flex gap-2 items-center">
+            <div class="w-[80%]">
+                <UInput icon="i-heroicons-folder-arrow-down" size="md" color="white" :trailing="false"
+                    placeholder="Add link to the product" />
+            </div>
+            <div>
+                <UButton color="primary" size="md" variant="solid">Add track</UButton>
+            </div>
+        </div>
+    </div>
+</template>
