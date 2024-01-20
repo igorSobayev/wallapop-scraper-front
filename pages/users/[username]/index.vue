1a96014226fd6b553a9eff09941db264e2a60428
Igor <isobayev@gmail.com>
2024-01-20 11:55:31 +0100

    Add new components to main view

--- pages/users/[username]/index.vue
+++ pages/users/[username]/index.vue
@@ -1,4 +1,6 @@
 <script setup>
+import WSNewTrack from '~/components/custom/WSNewTrack.vue'
+import WSTracks from '~/components/custom/WSTracks.vue'
 
 const route = useRoute()
 
@@ -7,18 +9,25 @@ useHead({
   meta: [
     {
       name: 'description',
-      content: 'TODO: SEO' 
+      content: 'TODO: SEO'
     },
   ],
 })
 
 onNuxtReady(async () => {
-    
+
 })
 </script>
 
 <template>
-    <div class="flex justify-center flex-col gap-2 pt-5">
-      Yay
+  <div class="flex justify-center flex-col gap-2 pt-5">
+    <!-- Add new track -->
+    <div class="mb-4">
+      <WSNewTrack />
     </div>
+    <!-- Track table -->
+    <div class="mt-4">
+      <WSTracks />
+    </div>
+  </div>
 </template>
