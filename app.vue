7c3f77d63c9b6d986ce90b65358ae21458bdc3ad
Igor <isobayev@gmail.com>
2024-01-20 11:54:43 +0100

    Remove bg color

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-[#eeeeee] h-screen flex flex-col overflow-x-hidden">
+  <div class="h-screen flex flex-col overflow-x-hidden">
     <NKNavBar />
     <div class="flex justify-center pt-20 mb-auto">
       <NuxtPage class="xl:w-2/3 w-[90%]" />
