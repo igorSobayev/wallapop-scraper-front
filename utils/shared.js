8a497988e0494ea2e9820798b4ed58ff6a3241b4
Igor <isobayev@gmail.com>
2024-01-29 18:12:02 +0100

    Create shared

---
+++ utils/shared.js
@@ -0,0 +1,25 @@
+// Shared params
+const config = {
+    ROLES: {
+        'USER': 'user',
+        'ADMIN': 'admin',
+    },
+    PLANS: {
+        'FREE': 'free',
+        'BASIC': 'basic',
+        'MEDIUM': 'medium',
+        'UNLIMITED': 'unlimited',
+    },
+    PLANS_CONFIG: {
+        'DAILY': 'daily',
+        'TWICE': 'twice',
+        'SIX_HOURS': 'six_hours',
+        'THREE_HOURS': 'three_hours',
+        'ONE_HOUR': 'one_hour',
+    },
+    AVAILABLE_PLATFORMS: {
+        'WALLAPOP': 'wallapop',
+    },
+}
+
+export default config
