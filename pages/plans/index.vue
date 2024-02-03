4cb08e2813775b761538102ed248dad941d8d8b0
Igor <isobayev@gmail.com>
2024-02-03 23:32:28 +0100

    Plans page

---
+++ pages/plans/index.vue
@@ -0,0 +1,27 @@
+<script setup>
+import WSPlansTable from '~/components/custom/WSPlansTable.vue'
+
+const route = useRoute()
+
+const tracksComp = ref(null)
+
+useHead({
+  title: 'Plans',
+  meta: [
+    {
+      name: 'description',
+      content: 'TODO: SEO'
+    },
+  ],
+})
+
+onNuxtReady(async () => {
+
+})
+</script>
+
+<template>
+  <div class="flex justify-center flex-col gap-2 pt-5">
+    <WSPlansTable />
+  </div>
+</template>
