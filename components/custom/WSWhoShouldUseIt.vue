98ccb8342def09529419b92b45cc8b3a2153603d
Igor <isobayev@gmail.com>
2024-02-12 18:29:49 +0100

    Who should use it component

---
+++ components/custom/WSWhoShouldUseIt.vue
@@ -0,0 +1,55 @@
+<script setup></script>
+
+
+<template>
+    <div class="flex flex-col items-center">
+        <h3 class="text-sm font-bold bg-gradient-to-r from-amber-500 to-pink-500 bg-clip-text text-transparent">¿Quién debería de utilizarla?</h3>
+        <div class="mb-5 text-lg">El tracker es ideal para todo tipo de perfiles</div>
+        <div class="flex flex-row justify-center mt-5 w-full">
+            <div class="w-[30%]">
+                <h4 class="text-3xl mb-4">Compadores</h4>
+                <div class="leading-5 flex flex-col gap-3">
+                    <p>
+                        ¿Quieres estar siempre al tanto de las actualizaciones de los productos que te intersan en Wallapop?
+                    </p>
+                    <p>
+                        Simplemente introduce el enlace del producto que te interesa, y recibe información actualizada sobre cambios en su precio, visitas, favoritos y disponibilidad.
+                    </p>
+                    <p>
+                        ¡Ahorra tiempo y no te pierdas ninguna oportunidad con Wallatracker!
+                    </p>
+                </div>
+                <div class="mt-4">
+                    <div class="flex items-center gap-1 mb-2"><UIcon name="i-heroicons-check text-green-500 text-md" /> Actualizaciones constantes</div>
+                    <div class="flex items-center gap-1 mb-2"><UIcon name="i-heroicons-check text-green-500 text-md" /> Decisiones informadas</div>
+                    <div class="flex items-center gap-1 mb-2"><UIcon name="i-heroicons-check text-green-500 text-md" /> Ahorro de tiempo</div>
+                    <div class="flex items-center gap-1 mb-2"><UIcon name="i-heroicons-check text-green-500 text-md" /> Exploración eficiente</div>
+                </div>
+            </div>
+            <div class="w-[10%]"><UDivider class="h-full" label="OR" orientation="vertical" /></div>
+            <div class="w-[30%]">
+                <h4 class="text-3xl mb-4">Vendedores</h4>
+                <div class="leading-5 flex flex-col gap-3">
+                    <p>
+                        ¿Quieres gestionar todos tus productos a la venta desde un mismo panel?
+                    </p>
+                    <p>
+                        Con Wallatracker podrás tomar las mejores decisiones para saber si tu producto esta bien configurado.
+                    </p>
+                    <p>
+                        Gracias a las actualizaciones constantes, podrás saber las tendencias y si tu producto se esta siendo visitado.
+                    </p>
+                    <p>
+                        ¡Ahorra tiempo y toma mejores decisiones!
+                    </p>
+                </div>
+                <div class="mt-4">
+                    <div class="flex items-center gap-1 mb-2"><UIcon name="i-heroicons-check text-green-500 text-md" /> Ahorro de tiempo</div>
+                    <div class="flex items-center gap-1 mb-2"><UIcon name="i-heroicons-check text-green-500 text-md" /> Gestión eficiente</div>
+                    <div class="flex items-center gap-1 mb-2"><UIcon name="i-heroicons-check text-green-500 text-md" /> Decisiones inteligentes</div>
+                    <div class="flex items-center gap-1 mb-2"><UIcon name="i-heroicons-check text-green-500 text-md" /> Velocidad de reacción</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
