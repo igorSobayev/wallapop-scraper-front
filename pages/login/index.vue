d57623166266073a545c2e177b5fbd8ac2bd4f35
Igor <isobayev@gmail.com>
2024-02-17 18:04:26 +0100

    Add loading to login page

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -3,6 +3,7 @@ import { useAuthStore } from '../../store/auth'
 import { ref } from 'vue'
 import NKPasswordInput from '../../components/custom/NKPasswordInput.vue'
 import { useI18n } from 'vue-i18n'
+import { VueSpinnerBall } from 'vue3-spinners'
 
 const authStore = useAuthStore()
 const router = useRouter()
@@ -23,6 +24,8 @@ const state = ref({
   password: ''
 })
 
+const loading = ref(false)
+
 const validate = (state) => {
   const errors = []
   if (!state.email) errors.push({ path: 'email', message: t('required') })
@@ -34,13 +37,19 @@ const form = ref()
 
 async function submit () {
   await form.value.validate()
+  
+  loading.value = true
+
   authStore
     .login(state.value)
     .then((_response) => {
-      // TODO add loading
+      loading.value = false
       router.push("/")
     })
-    .catch((error) => console.log("API error", error))
+    .catch((error) => {
+      loading.value = false
+      console.log("API error", error)
+    })
 }
 
 function forgotPassword () {
@@ -50,7 +59,8 @@ function forgotPassword () {
 
 <template>
     <div class="flex justify-center items-center h-[80vh]">
-        <div class="bg-white p-5 rounded-md">
+        <div class="bg-white p-5 rounded-md flex flex-col items-center">
+            <VueSpinnerBall v-if="loading" size="24" color="primary" class="mb-3 text-center" />
             <UForm
             ref="form"
             :validate="validate"
@@ -58,17 +68,17 @@ function forgotPassword () {
             :validate-on="['submit']"
             @submit="submit"
             >
-            <UFormGroup label="Email" name="email">
-                <UInput type="email" name="email" v-model="state.email" />
-            </UFormGroup>
+              <UFormGroup label="Email" name="email">
+                  <UInput type="email" name="email" v-model="state.email" />
+              </UFormGroup>
 
-            <NKPasswordInput v-model="state.password" :label="$t('password')" name="password" class="mt-4" />
-        
-            <div class="mt-4 cursor-pointer text-primary" @click="forgotPassword">{{ $t('forgotPassword') }}</div>
+              <NKPasswordInput v-model="state.password" :label="$t('password')" name="password" class="mt-4" />
+          
+              <div class="mt-4 cursor-pointer text-primary" @click="forgotPassword">{{ $t('forgotPassword') }}</div>
 
-            <UButton type="submit" class="mt-4" block>
-                {{ $t('login') }}
-            </UButton>
+              <UButton type="submit" class="mt-4" block>
+                  {{ $t('login') }}
+              </UButton>
             </UForm>
         </div>
     </div>
