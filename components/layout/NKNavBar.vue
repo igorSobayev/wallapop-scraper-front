4e6ff7da258cef6083dc41807d44bb6c8d9dd122
Igor <isobayev@gmail.com>
2024-02-17 17:58:00 +0100

    Add new logo and favicon

--- components/layout/NKNavBar.vue
+++ components/layout/NKNavBar.vue
@@ -1,9 +1,9 @@
 <template>
     <header class="bg-white z-50 fixed w-screen border-b">
         <nav class="flex justify-between items-center w-[92%] mx-auto">
-            <div>
-                <img class="w-64 cursor-pointer" src="./../../assets/logo.png" alt="...">
-            </div>
+            <NuxtLink class="p-2" to="/">
+                <img class="w-52 cursor-pointer" src="./../../assets/logo.png" alt="...">
+            </NuxtLink>
             <div class="nav-links duration-500 md:static absolute bg-white md:min-h-fit min-h-[40vh] left-0 top-[-1000%] md:w-auto  w-full flex items-center px-5 z-50" :class="{ 'top-[101%]': menuOpenned }">
                 <ul class="flex md:flex-row flex-col md:items-center md:gap-[4vw] gap-8" v-if="authStore.isLoggedIn">
                     <NuxtLink class="hover:text-gray-500" to="/">{{ $t('home') }}</NuxtLink>
