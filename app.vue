47b18057669a320c3e7d5fdd491de18ebd04f130
Igor <isobayev@gmail.com>
2024-02-22 20:22:22 +0100

    Responsive app

--- app.vue
+++ app.vue
@@ -2,7 +2,7 @@
   <div class="h-screen flex flex-col overflow-x-hidden">
     <NKNavBar />
     <div class="flex justify-center pt-20 mb-auto">
-      <NuxtPage class="xl:w-2/3 w-[90%]" />
+      <NuxtPage class="2xl:w-2/3 w-[90%]" />
     </div>
     <NKFooter />
     <UNotifications />
